13a7c49b5eb07b316746a234333d07cc64488c41
yeheskielwonte <56017046+yeheskielwonte@users.noreply.github.com>
2022-02-17 19:36:19 +0800

    Initial Commit

---
+++ App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnboardScreen from './src/containers/organisms/Onboarding'
+import LoginOptionsScreen from './src/containers/organisms/LoginOptions';
+import UserScreen from './src/containers/organisms/Akun/User';
+import OwnerScreen from './src/containers/organisms/Akun/Owner';
+import Splash from './src/containers/organisms/Splash'
+import SignUpUser from './src/containers/organisms/Akun/SignUpUser';
+
+
+const Stack = createNativeStackNavigator();
+
+
+const Gopang=()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen
+          name='UserScreen'
+          component={UserScreen}
+        />
+        {/* <Stack.Screen
+          name='SignUpUser'
+          component={SignUpUser}
+        /> */}
+        {/* <Stack.Screen
+          name='Splash'
+          component={Splash}
+        />
+        <Stack.Screen
+          name='OnboardScreen'
+          component={OnboardScreen}
+        />
+        <Stack.Screen
+          name='LoginOptionsScreen'
+          component={LoginOptionsScreen}
+        />
+        <Stack.Screen
+          name='UserScreen'
+          component={UserScreen}
+        />
+        <Stack.Screen
+          name='OwnerScreen'
+          component={OwnerScreen}
+        /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Gopang;
+
+/*
+        <Stack.Screen
+          name='Splash'
+          component={Splash}
+        />
+        <Stack.Screen
+          name='OnboardScreen'
+          component={OnboardScreen}
+        />
+        <Stack.Screen
+          name='LoginOptionsScreen'
+          component={LoginOptionsScreen}
+        />
+*/
---
+++ Slides.js
@@ -0,0 +1,20 @@
+export default[
+    {
+        id:'1',
+        title: 'Find your Vacation Spot',
+        description:'Bla bla bla',
+        image: require('./Assets/Onboard1.png')
+    },
+    {
+        id:'2',
+        title: 'Find Your Place',
+        description: 'Choose your homestay, gazebo, and Enjoy it',
+        image: require('./Assets/Onboard2.png'),
+    },
+    {
+        id:'3',
+        title: 'Make a Special Moment',
+        description: 'Bla bla bla',
+        image: require('./Assets/Onboard1.png'),
+    }
+];
